6b62d06fba0e34ba9e7e431111293a109c6abf6a
walidtlm <tlemcani.waliid@gmail.com>
2023-10-04 15:11:06 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const session = require ('express-session');
-const passport = require('passport');
-const csurf = require ('csurf');
-const cookieparser = require ('cookie-parder');
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
 const app = express();
 
 
+
+
 const blogPosts = [
     {
       id: 1,
-      title: 'First Blog Post',
+      title: 'The Breaking Bad',
       content: 'This is the content of the first blog post.',
       author: 'John Doe',
       date: '2023-09-15',
@@ -20,7 +19,7 @@ const blogPosts = [
     
     {
         id: 2,
-        title: 'Second Blog Post',
+        title: 'Suits',
         content: 'This is the content of the first blog post.',
         author: 'John Doe',
         date: '2023-09-15',
@@ -28,7 +27,7 @@ const blogPosts = [
 
       {
         id: 3,
-        title: ' Third Blog Post',
+        title: 'Peaky Blinders',
         content: 'This is the content of the first blog post.',
         author: 'John Doe',
         date: '2023-09-15',
@@ -37,29 +36,63 @@ const blogPosts = [
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  app.set('view engine', 'ejs')
+  app.set('views', './views')
 
   app.get("/", (req, res) => {
-    res.render("newBlogPost");
+    res.render('home', {blogPosts: blogPosts});
   })
+//get a post 
+app.get('/blogPosts/:id', (req,res) => {
+  const id = parseInt(req.params.id);
+  const blog = blogPosts.find((x) => x.id === id);
+if (blog){
+  res.render(blog);
+}else{
+  res.status(404).send('Nothing matches your research');
+}
+})
 
-  app.post("/", (req, res) => {
-    const { title, content } = req.body;
+//create new post
+  app.post("/blogPosts", (req, res) => {
+    const { title, content, author, date} = req.body;
     const newPost = {
-        id: blogPosts.length + 1; 
+        id: blogPosts.length +1, 
         title,
         content,
     }
     blogPosts.push(newPost);
-    res.redirect("/");
+    res.status(201).send('article added !!');
   })
+//modifier blog 
 
+app.put('/blogPosts/:id', (req, res) => {
+  const blogId = parseInt(req.params.id);
+  const blogIndex = blogPosts.findIndex((blog) => blog.id === blogId);
+
+  if (blogIndex !== -1) {
+    // Update the blog post with the new data from the request body
+    blogPosts[blogIndex] = {
+      ...blogPosts[blogIndex],
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+    };
+    res.redirect('/'); // Redirect to the home page after updating
+  } else {
+    res.status(404).send('Blog post not found');
+  }
+});
+
+
+//delete post 
   app.delete('/blogPosts/:id',(req,res) => {
     const blogId = parseInt(req.params.id);
-    const blogIndex = blogPosts.findIndex((article)=> blogPosts.id === blogId);
+    const blogIndex = blogPosts.findIndex((article)=> article.id === blogId);
     
     if(blogIndex !== -1) {
         blogPosts.splice(blogIndex, 1);
-        res.status(204).send('article delete')
+        res.status(204).send('article deleted')
     }else{
         res.status(404).send('article not found');
     }
