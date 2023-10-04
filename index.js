f04a6d7d187379741783dbb25160673e4dcd457b
walidtlm <tlemcani.waliid@gmail.com>
2023-10-04 09:19:28 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const session = require ('express-session');
 const passport = require('passport');
 const csurf = require ('csurf');
 const cookieparser = require ('cookie-parder');
+const bodyParser = require("body-parser");
+const PORT = 3000;
+
 const app = express();
 
 
@@ -32,28 +35,36 @@ const blogPosts = [
       },
   ];
 
-  const userProfiles = [
-    {
-      id: 1,
-      username: 'john_doe',
-      name: 'John Doe',
-      email: 'john@example.com',
-      // Other user profile information...
-    },
-    {
-        id: 2,
-        username: 'albert column',
-        name: 'John Doe',
-        email: 'john@example.com',
-        // Other user profile information...
-      },
-      {
-        id: 3,
-        username: 'david raw',
-        name: 'John Doe',
-        email: 'john@example.com',
-        // Other user profile information...
-      },
-  ];
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
+  app.get("/", (req, res) => {
+    res.render("newBlogPost");
+  })
 
+  app.post("/", (req, res) => {
+    const { title, content } = req.body;
+    const newPost = {
+        id: blogPosts.length + 1; 
+        title,
+        content,
+    }
+    blogPosts.push(newPost);
+    res.redirect("/");
+  })
+
+  app.delete('/blogPosts/:id',(req,res) => {
+    const blogId = parseInt(req.params.id);
+    const blogIndex = blogPosts.findIndex((article)=> blogPosts.id === blogId);
+    
+    if(blogIndex !== -1) {
+        blogPosts.splice(blogIndex, 1);
+        res.status(204).send('article delete')
+    }else{
+        res.status(404).send('article not found');
+    }
+  })
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`)
+})
